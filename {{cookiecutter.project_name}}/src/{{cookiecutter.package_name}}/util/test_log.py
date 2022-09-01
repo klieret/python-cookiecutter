6d8608a18dd3bd561f2de8cb98d07932bb6ddaaf
Kilian Lieret <kilian.lieret@posteo.de>
2022-09-01 13:59:22 -0400

    💥 Remove log level setter functions; test get_logger

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/util/test_log.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/util/test_log.py
@@ -1,7 +1,13 @@
 from __future__ import annotations
 
+import logging
+
 from {{cookiecutter.package_name}}.util.log import logger
 
 
 def test_logger():
     logger.info("Hello world")
+
+
+def test_get_logger():
+    assert isinstance(get_logger(), logging.Logger)
