d59a3b4bf08010a243397bad80aa712731cfb4fc
Kilian Lieret <kilian.lieret@posteo.de>
2022-11-09 10:26:46 -0500

    🐛 py3.7 doesn't have importlib.metadata

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -1,4 +1,9 @@
-import importlib.metadata
+from __future__ import annotations
+
+try:
+    from importlib.metadata import version
+except ModuleNotFoundError:
+    from importlib_metadata import version  # type: ignore
 
 
 __version__ = importlib.metadata.version('{{cookiecutter.project_name}}')
