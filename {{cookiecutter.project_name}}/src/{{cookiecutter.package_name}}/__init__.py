8dcba0a955c4f7f99e3d66ba75d064014c0fd755
Kilian Lieret <kilian.lieret@posteo.de>
2022-11-10 15:30:48 -0500

    🐛 Use imported; exclude conf.py from mypy

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -6,4 +6,4 @@ except ModuleNotFoundError:
     from importlib_metadata import version  # type: ignore
 
 
-__version__ = importlib.metadata.version('{{cookiecutter.project_name}}')
+__version__ = version('{{cookiecutter.project_name}}')
